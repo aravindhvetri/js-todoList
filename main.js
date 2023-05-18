a73d0b5ef07340d44bfaa64cbdb300ced194ef83
aravindhvetri <2002aravinthv@gmail.com>
2023-05-18 19:08:52 +0530

    ubdate

--- main.js
+++ main.js
@@ -15,18 +15,42 @@ function readpost(){
 }
 
 function createpost(){
+    let date = new Date;
     post.innerHTML += `<div class="div">
-      <h4 class="head-2">${data.text}</h4>
-      <span class="options">
-        <i onclick="editPost(this)" class="fa fa-edit"></i>
-        <i onclick="deletePost(this)" class="fa fa-trash"></i>
-      </span>  
+      <div class="col">
+        <h4 class="head-2">${data.text}</h4>
+        <span class="options">
+          <i onclick="editPost(this)" class="fa fa-edit"></i>
+          <i onclick="deletePost(this)" class="fa fa-trash"></i>
+        </span>
+      </div>
+      <div class="col">
+        <img class="image-1" src = "image/img-2.jpg" alt="no" width="200px" height="300px">
+        <div> TIME :${date.toLocaleTimeString()}</div>
+        <div> DATE: ${ date.toLocaleDateString()}</div>
+        <div class = "icons">
+          <div class ="ico"><i onclick="like(this)" class="fa fa-thumbs-up"></i></div>
+          <div class ="wrapper"><i onclick="heart(this)" class="fa fa-heart"></i></div>
+       </div>   
     </div>`
     input.value=" ";
 }
 function editPost(values){
    input.value = values.parentElement.previousElementSibling.innerHTML;
+   post.innerHTML="";
 }
 function deletePost(e){
-    e.parentElement.parentElement.remove();
+    e.parentElement.parentElement.parentElement.remove();   
+}
+function like(L){
+   L.style.transform = "scale(2.5,2.5)";
+   L.style.color = "blue";
+   L.style.transition = "1s";
+   L.parentElement.nextElementSibling.remove();
+}
+function heart(H){
+  H.style.transform = "scale(2.5,2.5)";
+  H.style.color = "red";
+  H.style.transition = "1s";
+  H.parentElement.previousElementSibling.remove();
 }
